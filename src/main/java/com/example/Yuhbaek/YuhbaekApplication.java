7432b9693cc78ba3da45b10c6057663b9c3ed0d3
yoonjumi <yjmjymyjm@gmail.com>
2026-01-19 16:24:55 +0900

    init: initial project structure

---
+++ src/main/java/com/example/Yuhbaek/YuhbaekApplication.java
@@ -0,0 +1,13 @@
+package com.example.Yuhbaek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuhbaekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuhbaekApplication.class, args);
+	}
+
+}
