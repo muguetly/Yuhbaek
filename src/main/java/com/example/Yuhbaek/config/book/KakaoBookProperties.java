29322e7a0510f56d4428625d250476d645e5cc1a
yoonyeorin <yeolin3579@naver.com>
2026-01-28 12:36:00 +0900

    팀원1 book 기능 제거

--- src/main/java/com/example/Yuhbaek/config/book/KakaoBookProperties.java
+++
@@ -1,9 +0,0 @@
-package com.example.Yuhbaek.config.book;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "kakao.book")
-public record KakaoBookProperties(
-        String restApiKey,
-        String baseUrl
-) {}
